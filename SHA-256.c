ba2c4e844d9d54c2eba31fbc269d90cd32c4b33d
Manas Ghai <manasg46@gmail.com>
2020-09-12 20:26:31 +0530

    Updated SHA-256.c

--- SHA-256.c
+++ SHA-256.c
@@ -191,6 +191,7 @@ the message length, to become the 512-bit padded message*/
 
 int main()
 {
+	int temp;
 	unchar text[] = {"abc"};
 	unchar hash[BLOCK_SIZE] = {0xba,0x78,0x16,0xbf,0x8f,0x01,0xcf,0xea,0x41,0x41,0x40,0xde,0x5d,0xae,0x22,0x23,
 	                                 0xb0,0x03,0x61,0xa3,0x96,0x17,0x7a,0x9c,0xb4,0x10,0xff,0x61,0xf2,0x00,0x15,0xad};
@@ -206,13 +207,22 @@ int main()
 	//Check If both Hash Values are same
 	pass = pass && !memcmp(hash, buffer, BLOCK_SIZE);
 	for(int idx = 0; idx < BLOCK_SIZE; idx++){
+		temp = buffer[idx];
+		if(temp < 16)
+			printf("0"); // so that first 0 is not ignored
+
 		printf("%x", buffer[idx]);
 	}
 	printf("\n");
 	for(int idx = 0; idx < BLOCK_SIZE; idx++){
+		temp = hash[idx];
+		if(temp < 16)
+			printf("0"); // so that first 0 is not ignored
+
 		printf("%x", hash[idx]);
 	}
 	printf("\n");
+
 
 	printf("SHA-256 test: %s\n", pass ? "SUCCEEDED" : "FAILED");
 	return 0;
